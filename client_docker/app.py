b74398c30068568374c639e93e753a3633eb4377
Nabeel Noor <nabeelnoor914@gmail.com>
2022-05-20 03:50:40 +0500

    changing port to 8080 for client and exposing it will work on azure web services(docker container)

--- client_docker/app.py
+++ client_docker/app.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
     # logging.basicConfig()
     # run()
     # app.run(debug=True)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
 
 
